fbb332402b0f30b797a4257e8be9a8923243de9f
Paulius Kaminskas <paulius.kaminskas@nfq.lt>
2019-01-11 16:33:56 +0200

    Run invalidator on prePersist too

--- EventListener/SeoInvalidateSubscriber.php
+++ EventListener/SeoInvalidateSubscriber.php
@@ -33,51 +33,66 @@ class SeoInvalidateSubscriber implements EventSubscriber
     public function getSubscribedEvents(): array
     {
         return [
+            'prePersist',
             'postUpdate',
             'preRemove',
         ];
     }
 
+    public function prePersist(LifecycleEventArgs $event): void
+    {
+        $this->invalidate($event, __METHOD__);
+    }
+
     public function postUpdate(LifecycleEventArgs $event): void
     {
-        $this->invalidate($event);
+        $this->invalidate($event, __METHOD__);
     }
 
     public function preRemove(LifecycleEventArgs $event): void
     {
-        $this->remove($event);
+        $this->remove($event, __METHOD__);
     }
 
-    private function remove(LifecycleEventArgs $event): void
+    private function remove(LifecycleEventArgs $event, string $eventName): void
     {
         $entity = $event->getEntity();
 
         try {
-            $invalidator = $this->getInvalidator($entity);
+            $invalidator = $this->getInvalidator($entity, $eventName);
 
             $invalidator->remove($entity);
         } catch (\InvalidArgumentException $ex) {
             //Invalidator was not found, so just continue
         }
     }
-    
-    private function invalidate(LifecycleEventArgs $event): void
+
+    private function invalidate(LifecycleEventArgs $event, string $eventName): void
     {
         $entity = $event->getEntity();
 
         try {
-            $invalidator = $this->getInvalidator($entity);
+            $invalidator = $this->getInvalidator($entity, $eventName);
 
             $uow = $event->getEntityManager()->getUnitOfWork();
 
-            $invalidator->invalidate($entity, $uow->getEntityChangeSet($entity));
+            $changeSet = $uow->getEntityChangeSet($entity);
+            // If changeset is empty set it to null
+            if (empty($changeSet)) {
+                $changeSet = null;
+            }
+
+            $invalidator->invalidate($entity, $changeSet);
         } catch (\InvalidArgumentException $ex) {
             //Invalidator was not found, so just continue
         }
     }
 
-    private function getInvalidator($entity): SeoInvalidatorInterface
+    private function getInvalidator($entity, string $eventName): SeoInvalidatorInterface
     {
-        return $this->invalidatorManager->getInvalidator(\get_class($entity));
+        return $this->invalidatorManager->getInvalidator(
+            \get_class($entity),
+            substr($eventName, strrpos($eventName, ':') + 1)
+        );
     }
 }
